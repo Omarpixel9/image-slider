827cea18d910c351cbd13f517f69d4e1b6d0e6e5
Omar Ahmad <superomar121@gmail.com>
2022-04-20 13:43:04 +0400

    added: css files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import imgs from './images.js';
+import './reset.css';
+import './style.css';
+import './image-slider.css';
+
 const contentDiv = document.querySelector('#content');
-const imageElement = document.createElement('img');
-contentDiv.appendChild(imageElement);
